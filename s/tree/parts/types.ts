5a609bd65d13040df8ef1655d892132c7e02667e
Chase Moskal <chasemoskal@gmail.com>
2025-06-29 19:39:46 -0700

    add: improved immutable/mutable types

--- s/tree/parts/types.ts
+++ s/tree/parts/types.ts
@@ -18,7 +18,14 @@ export type Versioned<S extends Treestate> = {
 
 export type Immutable<T> =
 	T extends (...args: any[]) => any ? T :
+	T extends readonly any[] ? ReadonlyArray<Immutable<T[number]>> :
 	T extends object ? { readonly [K in keyof T]: Immutable<T[K]> } :
+	T
+
+export type Mutable<T> =
+	T extends (...args: any[]) => any ? T :
+	T extends ReadonlyArray<infer U> ? Mutable<U>[] :
+	T extends object ? { -readonly [K in keyof T]: Mutable<T[K]> } :
 	T
 
 export type Tree<S extends Branchstate> = {
