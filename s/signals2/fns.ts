30daff33a49b4f98256fdf2684a082150501dac8
Chase Moskal <chasemoskal@gmail.com>
2025-09-03 02:09:12 -0700

    fix: fns options

--- s/signals2/fns.ts
+++ s/signals2/fns.ts
@@ -2,17 +2,27 @@
 import {Lazy} from "./lazy.js"
 import {Signal} from "./signal.js"
 import {Derive} from "./derive.js"
+import {SignalOptions} from "./types.js"
 
-export function lazy<V>(formula: () => V) {
-	return new Lazy<V>(formula)
+export function lazy<V>(
+		formula: () => V,
+		options?: Partial<SignalOptions>,
+	) {
+	return new Lazy<V>(formula, options)
 }
 
-export function derive<V>(formula: () => V) {
-	return new Derive<V>(formula)
+export function derive<V>(
+		formula: () => V,
+		options?: Partial<SignalOptions>,
+	) {
+	return new Derive<V>(formula, options)
 }
 
-export function signal<V>(value: V) {
-	return new Signal<V>(value)
+export function signal<V>(
+		value: V,
+		options?: Partial<SignalOptions>,
+	) {
+	return new Signal<V>(value, options)
 }
 
 signal.lazy = lazy
