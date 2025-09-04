5cab0f781527717705782d9a3d7ef00ccd40b2bd
Chase Moskal <chasemoskal@gmail.com>
2025-09-03 23:15:29 -0700

    tidy: imports

--- s/signals/signal.ts
+++ s/signals/signal.ts
@@ -1,9 +1,9 @@
 
 import {SignalOptions} from "./types.js"
+import {hipster} from "./utils/hipster.js"
 import {Reactive} from "./parts/reactive.js"
 import {tracker} from "../tracker/tracker.js"
 import {defaultCompare} from "./utils/default-compare.js"
-import { hipster } from "./utils/hipster.js"
 
 export class Signal<V> extends Reactive<V> {
 	#lock = false
