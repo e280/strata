0e21602d88fd75bd8328c60ea2a5d4d27f60fa77
Chase Moskal <chasemoskal@gmail.com>
2026-04-10 23:06:00 -0700

    improve: wait types and helpers

--- s/wait/parts/is.ts
+++ s/wait/parts/is.ts
@@ -1,8 +1,8 @@
 
-import {Wait} from "./type.js"
+import {Wait, WaitDone, WaitErr, WaitOk, WaitPending} from "./type.js"
 
-export const isWaitPending = (wait: Wait<unknown>) => !wait.done
-export const isWaitDone = (wait: Wait<unknown>) => wait.done
-export const isWaitOk = (wait: Wait<unknown>) => wait.done && wait.ok
-export const isWaitErr = (wait: Wait<unknown>) => wait.done && !wait.ok
+export const isWaitPending = (wait: Wait<unknown>): wait is WaitPending => !wait.done
+export const isWaitDone = <Value, E = unknown>(wait: Wait<Value, E>): wait is WaitDone<Value, E> => wait.done
+export const isWaitOk = <Value>(wait: Wait<Value, unknown>): wait is WaitOk<Value> => wait.done && wait.ok
+export const isWaitErr = <E = unknown>(wait: Wait<unknown, E>): wait is WaitErr<E> => wait.done && !wait.ok
 
--- s/wait/parts/type.ts
+++ s/wait/parts/type.ts
@@ -1,9 +1,11 @@
 
-import {Result} from "@e280/stz"
+import {Err, Ok, Result} from "@e280/stz"
 import {Derived} from "../../signals/index.js"
 
 export type WaitPending = {done: false}
 export type WaitDone<Value, E = unknown> = {done: true} & Result<Value, E>
+export type WaitOk<Value> = {done: true} & Ok<Value>
+export type WaitErr<E = unknown> = {done: true} & Err<E>
 
 export type Wait<Value, E = unknown> =
 	| WaitPending
